4bc6a68ce38e26ec086d400233556f753bebbac2
xLiwiax <boliwia333@gmail.com>
2022-03-27 19:04:41 +0200

    Zadanie

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    console.log("Zadanie 36787")
+
+    $('.accordion-item').click(function (){
+        if($(this).hasClass('open')){
+            $(this).removeClass('open');
+            $(this).find('.accordion-content').slideUp()
+        } else {
+            $(this).addClass('open');
+            $(this).find('.accordion-content').slideDown();
+        }
+        
+    });
+
+});
